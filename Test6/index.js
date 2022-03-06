379ad6ec8193ab6fc6b7f246ca282f9c2927cf74
SandhyaKamurti <96307063+SandhyaKamurti@users.noreply.github.com>
2022-03-06 21:34:15 +0530

    Update index.js

--- Test6/index.js
+++ Test6/index.js
@@ -1,4 +1,40 @@
-function show() {
-    var unsub = document.getElementsByClassName('show');
-    unsub.innerHTML = "<button>show</button>";
+function myFunction() {
+    var popup = document.getElementById("myPopup");
+    popup.classList.toggle("show");
+}
+
+
+function myFunction2() {
+    var popup2 = document.getElementById("myPopup2");
+    popup2.classList.toggle("show");
+}
+
+function myFunction3() {
+    var popup3 = document.getElementById("myPopup3");
+    popup3.classList.toggle("show");
+}
+
+function myFunction4() {
+    var popup4 = document.getElementById("myPopup4");
+    popup4.classList.toggle("show");
+}
+
+function myFunction5() {
+    var popup5 = document.getElementById("myPopup5");
+    popup5.classList.toggle("show");
+}
+
+function myFunction6() {
+    var popup6 = document.getElementById("myPopup6");
+    popup6.classList.toggle("show");
+}
+
+function myFunction7() {
+    var popup7 = document.getElementById("myPopup7");
+    popup7.classList.toggle("show");
+}
+
+function myFunction8() {
+    var popup8 = document.getElementById("myPopup8");
+    popup8.classList.toggle("show");
 }
